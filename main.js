78137b0bac35d84c8b6c1ad9749417f18380088d
LL-Dechema <64421166+LL-Dechema@users.noreply.github.com>
2020-05-05 14:54:43 +0200

    Update main.js

--- main.js
+++ main.js
@@ -1,6 +1,6 @@
 /* Create variable to hold map element, give initial settings to map */
 
-    var map = L.map('map',{ center: [51.65892, 6.41601], zoom: 5});
+    var map = L.map('map',{ center: [51.65892, 6.41601], zoom: 3});
 
     /* Add OpenStreetMap tile layer to map element */
 
