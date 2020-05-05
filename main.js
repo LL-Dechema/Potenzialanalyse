0560013e1a27ca50743f06ed5abe7256a0565396
LL-Dechema <64421166+LL-Dechema@users.noreply.github.com>
2020-05-05 14:51:22 +0200

    Create main.js

---
+++ main.js
@@ -0,0 +1,7 @@
+/* Create variable to hold map element, give initial settings to map */
+
+    var map = L.map('map',{ center: [51.65892, 6.41601], zoom: 10});
+
+    /* Add OpenStreetMap tile layer to map element */
+
+    L.tileLayer('http://{s}.tile.osm.org/{z}/{x}/{y}.png', { attribution: '© OpenStreetMap' }).addTo(map);
