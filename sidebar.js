60557b6ff9bdae94829c7c30626aac93e2045e33
LL-Dechema <64421166+LL-Dechema@users.noreply.github.com>
2020-05-05 16:37:00 +0200

    Update sidebar.js

--- sidebar.js
+++ sidebar.js
@@ -2,14 +2,27 @@
 		xmlns="http://www.w3.org/2000/svg">
 		<defs>
 			
-			/*Symbol definition */
-			<symbol id="icon-refinery" viewbox="0 0 32 32">
-				<title>refinery</title>
-				<script src="symbols/refinery.svg" defer></script>
-			</symbol>
-
-	
-
+		/*Symbol definition */
+		<symbol id="icon-refinery" viewbox="0 0 32 32">
+			<title>refinery</title>
+			<script src="symbols/Refinery.svg" defer></script>
+		</symbol>
+	<div id="sidebar" class="sidebar">
+		/* Nav tabs */
+		
+		<div class="sidebar-tabs">
+			<ul role="tablist" id="maintablist">
+				<li id="refinery-tab-li">
+					<a href="#refinery-content" role="tab" aria-label="Filter by refineries">
+						<svg class="svgicon" viewbox="0 0 32 32">
+							<use xlink:href="#icon-refinery"></use>
+						</svg>
+					</a>
+				</li>
+			</ul>
+		</div>
+		/*Tab panes */
+		
 		</defs>
-
+	</div>
 	</svg>
